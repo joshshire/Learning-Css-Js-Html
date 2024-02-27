241824b5fbf543882efe0b3b8c779ed5c24fed2e
joshshire <70442635+joshshire@users.noreply.github.com>
2024-02-27 13:12:46 -0700

    Delete Random number generator directory

--- Random number generator/Script.js
+++
@@ -1,9 +0,0 @@
-let random;
-const min = 50;
-const max = 100;
-document.getElementById('RandBtn').onclick = function(){
-    random = Math.floor(Math.random() * (max - min)) + min;
-
-    console.log(random);
-    document.getElementById('RandNum').textContent = random;
-};
